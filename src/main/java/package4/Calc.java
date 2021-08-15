2d62c57a2203c802c49df59ee5caa8c896f79299
Amy Luo <lvh.bag@gmail.com>
2021-08-15 15:49:25 +1200

    Initial Files II

---
+++ src/main/java/package4/Calc.java
@@ -0,0 +1,23 @@
+package package4;
+
+public class Calc {
+
+
+
+ public static void main( String[] args )
+    {
+  
+    }
+  public Calc() {
+     }
+
+  public int add(int x, int y) {
+     return x + y;
+  }
+
+ public int subtract(int x, int y) {
+     return x + y;
+  }
+
+
+}
---
+++ src/test/java/package4/TestCalc.java
@@ -0,0 +1,17 @@
+package package4;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestCalc extends TestCase {  
+
+
+  Calc obj=new Calc();  
+  @Test
+    public void testAdd() {  
+      
+        assertEquals(25,obj.add(10, 15));  
+    }  
+}  
